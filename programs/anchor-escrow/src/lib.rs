543c5f398c7f3efb6c4912f9ec0d6bbe278dde20
smilish67 <smilish67@naver.com>
2022-06-01 10:19:16 +0900

    survey_contract 0.1v

--- programs/anchor-escrow/src/lib.rs
+++ programs/anchor-escrow/src/lib.rs
@@ -2,13 +2,11 @@ use anchor_lang::prelude::*;
 use anchor_spl::token::{self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer};
 use spl_token::instruction::AuthorityType;
 
-declare_id!("6m7hFFRbDoCAN5bTm592crzaXNV3qkYwt6aaEzd1rkg6");
+declare_id!("2m9zddeqaVarWy3MHk59BSTZ6xF8jeWuHZCRXvQw5Noh");
 
 #[program]
-pub mod anchor_escrow {
+pub mod survey_rewards {
     use super::*;
-
-    const ESCROW_PDA_SEED: &[u8] = b"escrow";
 
     pub fn initialize(
         ctx: Context<Initialize>,
@@ -24,18 +22,13 @@ pub mod anchor_escrow {
             .initializer_deposit_token_account
             .to_account_info()
             .key;
-        ctx.accounts
-            .escrow_account
-            .initializer_receive_token_account = *ctx
-            .accounts
-            .initializer_receive_token_account
-            .to_account_info()
-            .key;
         ctx.accounts.escrow_account.initializer_amount = initializer_amount;
         ctx.accounts.escrow_account.taker_amount = taker_amount;
 
+        let escrow_pda_seed : &[u8] = ctx.accounts.escrow_account.to_account_info().key.as_ref();
+
         let (vault_authority, _vault_authority_bump) =
-            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+            Pubkey::find_program_address(&[escrow_pda_seed], ctx.program_id);
         token::set_authority(
             ctx.accounts.into_set_authority_context(),
             AuthorityType::AccountOwner,
@@ -50,10 +43,12 @@ pub mod anchor_escrow {
         Ok(())
     }
 
-    pub fn cancel(ctx: Context<Cancel>) -> Result<()> {
+    pub fn cancel(ctx: Context<Cancel>, ) -> Result<()> {
+        let escrow_pda_seed : &[u8] = ctx.accounts.escrow_account.to_account_info().key.as_ref();
         let (_vault_authority, vault_authority_bump) =
-            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
-        let authority_seeds = &[&ESCROW_PDA_SEED[..], &[vault_authority_bump]];
+            Pubkey::find_program_address(&[escrow_pda_seed], ctx.program_id);
+        let authority_seeds = &[&escrow_pda_seed[..], &[vault_authority_bump]];
+
 
         token::transfer(
             ctx.accounts
@@ -72,27 +67,23 @@ pub mod anchor_escrow {
     }
 
     pub fn exchange(ctx: Context<Exchange>) -> Result<()> {
+        let escrow_pda_seed : &[u8] = ctx.accounts.escrow_account.to_account_info().key.as_ref();
         let (_vault_authority, vault_authority_bump) =
-            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
-        let authority_seeds = &[&ESCROW_PDA_SEED[..], &[vault_authority_bump]];
-
-        token::transfer(
-            ctx.accounts.into_transfer_to_initializer_context(),
-            ctx.accounts.escrow_account.taker_amount,
-        )?;
+            Pubkey::find_program_address(&[escrow_pda_seed], ctx.program_id);
+        let authority_seeds = &[&escrow_pda_seed[..], &[vault_authority_bump]];
 
         token::transfer(
             ctx.accounts
                 .into_transfer_to_taker_context()
                 .with_signer(&[&authority_seeds[..]]),
-            ctx.accounts.escrow_account.initializer_amount,
+            ctx.accounts.escrow_account.taker_amount,
         )?;
 
-        token::close_account(
-            ctx.accounts
-                .into_close_context()
-                .with_signer(&[&authority_seeds[..]]),
-        )?;
+        // token::close_account(
+        //     ctx.accounts
+        //         .into_close_context()
+        //         .with_signer(&[&authority_seeds[..]]),
+        // )?;
 
         Ok(())
     }
@@ -107,7 +98,7 @@ pub struct Initialize<'info> {
     pub mint: Account<'info, Mint>,
     #[account(
         init,
-        seeds = [b"token-seed".as_ref()],
+        seeds = [escrow_account.to_account_info().key.as_ref()],
         bump,
         payer = initializer,
         token::mint = mint,
@@ -119,7 +110,6 @@ pub struct Initialize<'info> {
         constraint = initializer_deposit_token_account.amount >= initializer_amount
     )]
     pub initializer_deposit_token_account: Account<'info, TokenAccount>,
-    pub initializer_receive_token_account: Account<'info, TokenAccount>,
     #[account(zero)]
     pub escrow_account: Box<Account<'info, EscrowAccount>>,
     /// CHECK: This is not dangerous because we don't read or write from this account
@@ -171,7 +161,6 @@ pub struct Exchange<'info> {
         mut,
         constraint = escrow_account.taker_amount <= taker_deposit_token_account.amount,
         constraint = escrow_account.initializer_deposit_token_account == *initializer_deposit_token_account.to_account_info().key,
-        constraint = escrow_account.initializer_receive_token_account == *initializer_receive_token_account.to_account_info().key,
         constraint = escrow_account.initializer_key == *initializer.key,
         close = initializer
     )]
@@ -188,7 +177,6 @@ pub struct Exchange<'info> {
 pub struct EscrowAccount {
     pub initializer_key: Pubkey,
     pub initializer_deposit_token_account: Pubkey,
-    pub initializer_receive_token_account: Pubkey,
     pub initializer_amount: u64,
     pub taker_amount: u64,
 }
@@ -241,20 +229,6 @@ impl<'info> Cancel<'info> {
 }
 
 impl<'info> Exchange<'info> {
-    fn into_transfer_to_initializer_context(
-        &self,
-    ) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
-        let cpi_accounts = Transfer {
-            from: self.taker_deposit_token_account.to_account_info().clone(),
-            to: self
-                .initializer_receive_token_account
-                .to_account_info()
-                .clone(),
-            authority: self.taker.clone(),
-        };
-        CpiContext::new(self.token_program.clone(), cpi_accounts)
-    }
-
     fn into_transfer_to_taker_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
         let cpi_accounts = Transfer {
             from: self.vault_account.to_account_info().clone(),
